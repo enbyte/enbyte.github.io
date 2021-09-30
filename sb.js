bbffe7c221b0efd3c7d59ce51933ce49a2a3a5dd
Enbyte Games <83731959+enbyte@users.noreply.github.com>
2021-09-30 08:06:00 -0400

    Update sb.js

--- sb.js
+++ sb.js
@@ -4,18 +4,45 @@ import 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
 var hypixel_api_prefix = 'https://api.hypixel.net/';
 var bazaar_postfix = 'skyblock/bazaar';
 var b_url = hypixel_api_prefix + bazaar_postfix;
+var bazaar_data = undefined;
 
-function didWork(data, status) {
+String.prototype.ucwords = function() {
+  str = this.toLowerCase();
+  return str.replace(/(^([a-zA-Z\p{M}]))|([ -][a-zA-Z\p{M}])/g,
+  	function(s){
+  	  return s.toUpperCase();
+	});
+};
+
+function prettify(name) {
+  return name.replace('_', ' ').ucwords();
+}
+
+function did_work(data, status) {
   if (o.status == 200 && data.success) {
-    display(data);
-    return;
+    bazaar_data = data;
+    return true;
   } else {
   console.log('Request to API servers failed with code ' + status);
   send_bazaar_request();
   }
 }
 
-function display(data, body_object) {  /* todo */  };
+function add_child_p(object, text) {
+  p = document.createElement('p');
+  p.innerHTML = text; // ok a little cheap here
+  object.appendChild(p);
+  return p;
+}
+
+function display(data, body_object) {  /* todo */
+  var products = data.products.keys();
+  var d = data.products;
+  var k = products;
+  for (var i = 0; i < products.length; i++) {
+    add_child_p(body_object, prettify(d[k[i]].product_id) + "'s buy price is " + String(d[k[i]].quick_status.sellPrice) + " coins."
+  };
+};
 
 function send_bazaar_request() {
   $.get(b_url, didWork);
